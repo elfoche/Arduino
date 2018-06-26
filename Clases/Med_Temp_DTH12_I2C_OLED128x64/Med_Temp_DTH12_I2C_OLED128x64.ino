823eab2991217d2962d05746cc9747a0455b94e2
elfoche <elfoche@gmail.com>
2018-06-25 22:29:48 -0300

    Anadando Sensor de temperatura + OLED

--- Clases/Med_Temp_DTH12_I2C_OLED128x64/Med_Temp_DTH12_I2C_OLED128x64.ino
+++ Clases/Med_Temp_DTH12_I2C_OLED128x64/Med_Temp_DTH12_I2C_OLED128x64.ino
@@ -1,14 +1,12 @@
-#include <DHT12.h>
+#include <Wire.h>
+
+#include <Adafruit_GFX.h>
 
 #include <Adafruit_SSD1306.h>
 
-#include <Adafruit_GFX.h>
-#include <Adafruit_SPITFT.h>
-#include <Adafruit_SPITFT_Macros.h>
-#include <gfxfont.h>
-
 #include "Arduino.h"
-#include <Wire.h>
+
+#include <DHT12.h>
 
 
 //Displa OLED 0.96" 128x64, Direccion I2C 0x3C
@@ -17,14 +15,13 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 // Set dht12 i2c comunication on default Wire pin
 DHT12 dht12;
-
-// Variable para el tiempo de sensado
-int timeSinceLastRead = 0;
  
 void setup(){
   Serial.begin(9600);
+  
   // Start sensor handshake
   dht12.begin();
+  
   //Star display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
@@ -35,7 +32,6 @@ void setup(){
 }
  
 void loop(){
-  if(timeSinceLastRead > 2000) {
     float t12 = dht12.readTemperature();
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
     float h12 = dht12.readHumidity(); 
@@ -43,17 +39,17 @@ void loop(){
     // Check if any reads failed and exit early (to try again).
     if (isnan(h12) || isnan(t12)) {
       Serial.println("Falla al leer el sensor DHT12!");
-  
+      dht12Read = false;
       // muestra una línea de texto
       display.setTextSize (2);
       display.setTextColor (WHITE);
       display.setCursor (1,1);
-      display.print ("Falla al leer el sensor DHT12!");
+      display.print ("Falla al  leer el   sensor DHT12!");
       display.display();
       delay(2000);
       display.clearDisplay();
       
-      dht12Read = false;
+
     }
   
   
@@ -62,6 +58,7 @@ void loop(){
       float hic12 = dht12.computeHeatIndex(t12, h12, false);
       // Compute dew point in Celsius (isFahreheit = false)
       float dpc12 = dht12.dewPoint(t12, h12, false);  
+  /*    
       Serial.print("Humedad: ");
       Serial.print(h12);
       Serial.println(" %\t");
@@ -75,23 +72,50 @@ void loop(){
       Serial.print(dpc12);
       Serial.println(" *C");
       Serial.println("###------------###");  
-
+*/
       // muestra una línea de texto
-      display.setTextSize (2);
-      display.setTextColor (WHITE);
-      display.setCursor (1,20);
-      display.print ("Temperatura:");
-      display.print (t12);
-      display.print ("*C");
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+     // display.startscrolldiagleft(1000,0);
+      
+      display.setCursor(0,0);
+      display.println("Temperatu");
+      display.print("ra:");
+      display.print(t12);      
+      display.println("\tC");
       display.display();
-      delay(2000);
       display.clearDisplay();
-   
-     
-    }
-    timeSinceLastRead = 0;
-  }
-delay(100);
-timeSinceLastRead += 100;
+      delay(2600);
 
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("Humedad:");
+      display.print(h12);
+      display.println("%");
+      display.display();
+      display.clearDisplay();
+      delay(2000);
+
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("S. Ter:");
+      display.print(hic12);
+      display.println (" \tC");
+      display.clearDisplay();
+      delay(2000);
+
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.println("P. Rocio:");
+      display.print(dpc12);
+      display.println(" \tC");      
+      display.display();
+      display.clearDisplay();
+      delay(2000);
+  
+    }
+    delay(2000);
 }
